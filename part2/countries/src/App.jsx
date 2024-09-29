7ac42d8b99324c09bc540c778ab9354556fb828b
Angeliki Fokou <a@fokou.gr>
2024-09-29 22:31:59 +0300

    Extract search into a separate module and improve behavior if user searches again

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,27 +1,6 @@
 import axios from 'axios'
 import { useState } from 'react';
-import Country from './Country';
-
-const Search = ({ countries }) => {
-  const [showCountry, setShowCountry] = useState(null);
-
-  if (showCountry) {
-    return <Country country={showCountry} />
-  }
-
-  return <>
-      { countries.length > 10
-      ? <p>Too many matches, specify another filter</p>
-      : countries.length > 1
-        ? countries.map((country) => <div key={country.name.common}>{country.name.common} <input type="button" value="show" onClick={() => {
-            setShowCountry(country)
-          }} /></div>)
-        : countries.length === 1
-          ? <Country country={countries[0]} />
-          : <p>No matches, specify another filter</p>
-      }
-  </>
-}
+import Search from './Search.jsx';
 
 const App = () => {
   const [searching, setSearching] = useState(false)
