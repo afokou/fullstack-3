248fbda7ed56f9ca7e1f7c5a090cab3a517923dc
Angeliki Fokou <a@fokou.gr>
2024-12-21 20:27:35 +0200

    Add error handler and convert the other endpoints

--- backend/index.js
+++ backend/index.js
@@ -41,16 +41,13 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id).then(result => {
       response.status(204).end()
-  }).catch(error => {
-    console.log(error)
-    response.status(500).end()
-  })
+  }).catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
   if (!body.name || !body.number) {
     return response.status(400).json({
@@ -65,28 +62,24 @@ app.post('/api/persons', (request, response) => {
 
   person.save().then(savedPerson => {
     response.json(savedPerson)
-  }).catch(error => {
-    console.log(error)
-    response.status(500).end()
-  })
+  }).catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
   const date = new Date()
-  response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
+  Person.find({}).then(persons => {
+    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
+  });
 })
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
   Person.findById(request.params.id).then(person => {
     if (person) {
       response.json(person)
     } else {
       response.status(404).end()
     }
-  }).catch(error => {
-    console.log(error)
-    response.status(500).end()
-  })
+  }).catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -94,6 +87,19 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
